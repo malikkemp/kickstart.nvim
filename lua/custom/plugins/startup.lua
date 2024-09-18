e48b34ce50195419702ece078e812a184399965d
Malik Kemp <malikkemp1152@gmail.com>
2024-09-17 22:27:46 -0400

    Updated starter page to use startup.nvim

---
+++ lua/custom/plugins/startup.lua
@@ -0,0 +1,22 @@
+return {
+  'startup-nvim/startup.nvim',
+  dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-file-browser.nvim' },
+  config = function()
+    require('startup').setup {
+      theme = 'dashboard', -- or "dashboard", "evil", "startify", or customize your own theme
+      options = {
+        header = {
+          [[Welcome Back, Malik]],
+          [[Custom startup page with startup.nvim]],
+        },
+        buttons = {
+          { 'e', '  New file', ':ene <BAR> startinsert <CR>' },
+          { 'f', '  Find file', ':Telescope find_files <CR>' },
+          { 'r', '  Recent files', ':Telescope oldfiles <CR>' },
+          { 'q', '  Quit Neovim', ':qa<CR>' },
+        },
+        footer = { 'Startup.nvim - Customized Neovim!' },
+      },
+    }
+  end,
+}
